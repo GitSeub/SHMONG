36385dee7000a31d724e21619691d976f9bcd8fd
Oscaro55 <oscaroduponchel@gmail.com>
2023-02-28 17:39:55 +0100

    Player Meca v1

---
+++ Assets/Script/Reflect.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Reflect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnCollisionEnter(Collision collision)
+     {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            print("oui");
+            Vector3 incomingVec = collision.relativeVelocity.normalized;
+            Vector3 normalVec = collision.contacts[0].normal;
+            var impactAngle = Vector3.Angle(incomingVec, normalVec);
+
+            var bullet = collision.gameObject.GetComponent<Bullet>();
+            bullet.Bounce(-impactAngle);
+        }
+     }
+}
